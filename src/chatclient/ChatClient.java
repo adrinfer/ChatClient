ab3bb3761e69dec3a1dd6ff735cdc16a7f4adfa6
adrinfer <adrinfer@adrinfer-PC>
2016-05-19 22:08:27 +0200

    ...

--- src/chatclient/ChatClient.java
+++ src/chatclient/ChatClient.java
@@ -9,6 +9,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 /**
@@ -17,14 +18,41 @@ import javafx.stage.Stage;
  */
 public class ChatClient extends Application {
     
+    private Scene scene;
+    private BorderPane root;
+    
+    
+    private void setBindings()
+    {
+        //Width
+        root.minWidthProperty().bind(scene.widthProperty().subtract(1));
+        root.maxWidthProperty().bind(scene.widthProperty().subtract(1));
+        root.prefWidthProperty().bind(scene.widthProperty().subtract(1));
+        
+        //Height
+        root.minHeightProperty().bind(scene.heightProperty().subtract(1));
+        root.maxHeightProperty().bind(scene.heightProperty().subtract(1));
+        root.prefHeightProperty().bind(scene.heightProperty().subtract(1));
+    }
+    
+    
+    //Cargar css utilizados - Debe ser llamado despues de crear la escena.
+    private void loadStyles()
+    {
+        scene.getStylesheets().add(getClass().getResource("/es/chatclient/styles/styles.css").toExternalForm());
+    }
+    
+    
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        root = FXMLLoader.load(getClass().getResource("/es/chatclient/views/clientGUI.fxml"));
         
-        Scene scene = new Scene(root);
-        
+        scene = new Scene(root);
+        loadStyles();
         stage.setScene(scene);
         stage.show();
+        
+        setBindings();
     }
 
     /**
--- src/es/chatclient/controllers/viewcontrollers/ClientGUIController.java
+++ src/es/chatclient/controllers/viewcontrollers/ClientGUIController.java
@@ -14,62 +14,142 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.animation.Timeline;
+import javafx.animation.TranslateTransition;
+import javafx.animation.TranslateTransitionBuilder;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.util.Duration;
 
 /**
  *
  * @author Practicas01
  */
 public class ClientGUIController implements Initializable {
+
+    @FXML
+    private BorderPane borderPane;
     
     @FXML
-    private Label label;
+    private Pane leftPane;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    private Button boton;
+    
+    @FXML
+    private Pane centralPane;
+    
+    
+    private TranslateTransition translateTransition;
+    
+    private boolean bol = true;
+    private double with;
     
     private void init()
     {
+        //split.setDividerPosition(1, 0.9);
         
         Gson gson = new Gson();
         
-        try 
-        {
-            Socket s = new Socket("localhost", 53013);
-            System.out.println("SOCKET CREADO");
-            DataOutputStream out = new DataOutputStream(s.getOutputStream());
-            System.out.println("OUTPUT CREADO");
+        leftPane.minWidthProperty().bind(borderPane.minWidthProperty().multiply(0.2));
+        leftPane.maxWidthProperty().bind(borderPane.maxWidthProperty().multiply(0.2));
+        leftPane.prefWidthProperty().bind(borderPane.prefWidthProperty().multiply(0.2));
+       
+        centralPane.minWidthProperty().bind(borderPane.minWidthProperty().subtract(leftPane.translateXProperty().add(leftPane.minWidthProperty())));
+        centralPane.maxWidthProperty().bind(borderPane.maxWidthProperty().subtract(leftPane.translateXProperty().add(leftPane.maxWidthProperty())));
+        centralPane.prefWidthProperty().bind(borderPane.prefWidthProperty().subtract(leftPane.translateXProperty().add(leftPane.prefWidthProperty())));
+        
+        centralPane.translateXProperty().bind(leftPane.translateXProperty().add(leftPane.prefWidthProperty()));
+        
+        
+        
+//          System.out.println("MIN: " + borderPane.minWidthProperty().getValue());
+//          System.out.println("MAX: " + borderPane.maxWidthProperty().getValue());
+//          System.out.println("PREF: " + borderPane.prefWidthProperty().getValue());
+
+//          centralPane.minWidthProperty().bind(borderPane.minWidthProperty());
+//          centralPane.maxWidthProperty().bind(borderPane.maxWidthProperty());
+//          centralPane.prefWidthProperty().bind(borderPane.prefWidthProperty());
+        
+//          System.out.println("CMIN: " + centralPane.minWidthProperty().getValue());          
+//          System.out.println("CMAX: " + centralPane.maxWidthProperty().getValue());
+//          System.out.println("CPREF: " + centralPane.prefWidthProperty().getValue());
+        //try 
+        //{
+            //Socket s = new Socket("localhost", 53013);
+            //System.out.println("SOCKET CREADO");
+            //DataOutputStream out = new DataOutputStream(s.getOutputStream());
+            //System.out.println("OUTPUT CREADO");
             //Request r = new Request(Request.LOGIN);
             //r.setUserData("adrinfer", "adrinfer", "adrinfer", "email");
             //out.writeUTF(gson.toJson(r));
             
             
             //a.writerFor(NetworkMessage.class).writeValues(s.getOutputStream()).write(new RegisterMessage());
-            System.out.println("MENSAJE ENVIADO");
+            //System.out.println("MENSAJE ENVIADO");
             
-            DataInputStream in = new DataInputStream(s.getInputStream());
+            //DataInputStream in = new DataInputStream(s.getInputStream());
             
-            System.out.println("ESPERANDO LECTURA");
-            String a = in.readUTF();
+            //System.out.println("ESPERANDO LECTURA");
+            //String a = in.readUTF();
             
-            System.out.println("LECTURA: " + a);
-            
-            
+           // System.out.println("LECTURA: " + a);
             
             
             
             
-        } 
-        catch (IOException ex) 
-        {
-            Logger.getLogger(ClientGUIController.class.getName()).log(Level.SEVERE, null, ex);
-        }
+            
+            
+       //} 
+        //catch (IOException ex) 
+        //{
+            //Logger.getLogger(ClientGUIController.class.getName()).log(Level.SEVERE, null, ex);
+        //}
+        
+        boton.setText("AAAA");
+        
+        
+        with = 0;
+               
+        translateTransition = TranslateTransitionBuilder.create()
+                .duration(Duration.seconds(4))
+                .node(leftPane)
+                .fromX(0)
+                .toX(0)
+                .cycleCount(Timeline.INDEFINITE)
+                .autoReverse(true)
+                .build();
+        
+        
+        boton.setOnAction(e -> {
+            System.out.println("X: " + leftPane.translateXProperty().getValue());
+            System.out.println("Xt: " + centralPane.translateXProperty().getValue() );
+            if(bol)
+            {
+                translateTransition.toXProperty().set(-leftPane.getWidth());
+               translateTransition.play(); 
+               bol = false;
+               
+               
+            }
+            else
+            {
+                translateTransition.stop();
+                bol = true;
+            }
+            
+            
+            
+            
+        });
+        
+        
     }
     
     @Override
